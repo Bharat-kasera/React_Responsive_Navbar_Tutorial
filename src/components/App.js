d89afd15e66f01cdca2b0a95f00987e1798bfb95
code_bender <92722527+Bharat-kasera@users.noreply.github.com>
2023-03-25 17:24:40 +0530

    Update App.js
     import Routes instead of Switch and wrap your <Route /> components with a <Routes> component.

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,11 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Navbar from "./Navbar/Navbar";
-import Home from "./pages/Home";
-import Demo from "./pages/Demo";
-import KeyFeatures from "./pages/KeyFeatures";
-import Pricing from "./pages/Pricing";
-import Testimonials from "./pages/Testimonials";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "../Navbar/Navbar";
+import Home from "../pages/Home";
+import Demo from "../pages/Demo";
+import KeyFeatures from "../pages/KeyFeatures";
+import Pricing from "../pages/Pricing";
+import Testimonials from "../pages/Testimonials";
 import "./App.css";
 
 function App() {
@@ -13,13 +13,13 @@ function App() {
     <div className="container">
       <Router>
         <Navbar />
-        <Switch>
+        <Routes>
           <Route path="/" exact component={Home} />
           <Route path="/features" exact component={KeyFeatures} />
           <Route path="/pricing" exact component={Pricing} />
           <Route path="/testimonials" exact component={Testimonials} />
           <Route path="/demo" exact component={Demo} />
-        </Switch>
+        </Routes>
       </Router>
     </div>
   );
